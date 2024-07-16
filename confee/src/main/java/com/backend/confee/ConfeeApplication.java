3321d0937deaac828ed2eb42d8a55073735ad16a
Viraji Dewmini <virajidewmini@gmail.com>
2024-07-16 21:06:10 +0530

    initial commit

---
+++ confee/src/main/java/com/backend/confee/ConfeeApplication.java
@@ -0,0 +1,13 @@
+package com.backend.confee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConfeeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConfeeApplication.class, args);
+	}
+
+}
---
+++ confee/src/test/java/com/backend/confee/ConfeeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.backend.confee;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfeeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
