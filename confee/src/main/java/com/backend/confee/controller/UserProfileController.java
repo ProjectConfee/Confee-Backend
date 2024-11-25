3181f08f0be0ab3c349dae00f6a678f256de11a3
Viranga Dias <2021cs050@stu.ucsc.cmb.ac.lk>
2024-11-25 21:44:03 +0530

    view profile details

--- confee/src/main/java/com/backend/confee/controller/UserProfileController.java
+++ confee/src/main/java/com/backend/confee/controller/UserProfileController.java
@@ -2,14 +2,14 @@ package com.backend.confee.controller;
 
 
 import com.backend.confee.dto.Announcement_spDTO;
+import com.backend.confee.entity.Announcement_sp;
 import com.backend.confee.service.AnnouncementService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("api/userProfiles")
@@ -28,5 +28,17 @@ public class UserProfileController {
             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Failed to add announcement");
         }
     }
+
+    @GetMapping("/getAll")
+    public ResponseEntity<?> getProfileDetails() {
+        try {
+            List<Announcement_sp> announcements = announcementService.getAllAnnouncements();
+            return ResponseEntity.ok(announcements);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Error fetching announcements");
+        }
+    }
+
 
 }
