f9a08a6ea97d3f36d02040bfd6603a10d46fdb26
KeshaliDhananjana <keshdhananjana@gmail.com>
2024-11-26 17:58:57 +0530

    view, update, delete

--- confee/src/main/java/com/backend/confee/controller/CommitteeController.java
+++ confee/src/main/java/com/backend/confee/controller/CommitteeController.java
@@ -5,6 +5,8 @@ import com.backend.confee.service.CommitteeService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
 @RequestMapping(value = "api/committee")
 @CrossOrigin
@@ -17,5 +19,20 @@ public class CommitteeController {
     @PostMapping("/saveCommittee")
     public CommitteeDTO saveCommittee(@RequestBody CommitteeDTO committeeDTO) {
         return committeeService.saveCommittee(committeeDTO);
+    }
+
+    @GetMapping("/getCommittee")
+    public List<CommitteeDTO> getCommittee() {
+        return committeeService.getAllCommittee();
+    }
+
+    @PutMapping("/updateCommittee")
+    public CommitteeDTO updateCommittee(@RequestBody CommitteeDTO committeeDTO) {
+        return committeeService.updateCommittee(committeeDTO);
+    }
+
+    @DeleteMapping("/deleteCommittee")
+    public boolean deleteCommittee(@RequestBody CommitteeDTO committeeDTO) {
+        return committeeService.deleteCommittee(committeeDTO);
     }
 }
