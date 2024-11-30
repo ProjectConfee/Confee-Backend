5483f0c0c973b992cd8dad203e3b0c5ffffa8ce8
ayeshgallege <ayeshpramuditha2000@gmail.com>
2024-11-30 15:26:47 +0530

    upgrade stall map

--- confee/src/main/java/com/backend/confee/dto/Stall_spDto.java
+++ confee/src/main/java/com/backend/confee/dto/Stall_spDto.java
@@ -30,6 +30,7 @@ package com.backend.confee.dto;
 import lombok.Getter;
 import lombok.Setter;
 
+
 @Getter
 @Setter
 public class Stall_spDto {
--- confee/src/main/java/com/backend/confee/service/Announcement_spServiceImpl.java
+++ confee/src/main/java/com/backend/confee/service/Announcement_spServiceImpl.java
@@ -100,6 +100,69 @@
 //
 //
 //}
+
+
+
+
+
+//bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb
+//package com.backend.confee.service;
+//
+//import com.backend.confee.dto.Announcement_spDTO;
+//import com.backend.confee.entity.Announcement_sp;
+//import com.backend.confee.repo.AnnouncementRepo;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.stereotype.Service;
+//
+//import java.time.LocalDateTime;
+//import java.util.List;
+//import java.util.Optional;
+//
+//@Service
+//public class Announcement_spServiceImpl implements AnnouncementService {
+//
+//    @Autowired
+//    private AnnouncementRepo announcementRepository;
+//
+//    @Override
+//    public Announcement_sp addAnnouncement(Announcement_spDTO announcementDTO) {
+//        Announcement_sp announcement = new Announcement_sp();
+//        announcement.setAnnouncement(announcementDTO.getAnnouncement());
+//        announcement.setTimestamp(LocalDateTime.now()); // Set the current timestamp
+//        return announcementRepository.save(announcement);
+//    }
+//
+//    @Override
+//    public List<Announcement_sp> getAllAnnouncements() {
+//        return announcementRepository.findAll();
+//    }
+//
+//    @Override
+//    public boolean updateAnnouncement(Long id, Announcement_spDTO announcementDTO) {
+//        Optional<Announcement_sp> optionalAnnouncement = announcementRepository.findById(id);
+//        if (optionalAnnouncement.isPresent()) {
+//            Announcement_sp announcement = optionalAnnouncement.get();
+//            announcement.setAnnouncement(announcementDTO.getAnnouncement());
+//            announcementRepository.save(announcement);
+//            return true;
+//        }
+//        return false;
+//    }
+//
+//    @Override
+//    public boolean deleteAnnouncement(Long id) {
+//        if (announcementRepository.existsById(id)) {
+//            announcementRepository.deleteById(id);
+//            return true;
+//        }
+//        return false;
+//    }
+//}
+
+
+
+//,mmmmmmmmmmmmmmmmmmmmmmmmmmm
+
 package com.backend.confee.service;
 
 import com.backend.confee.dto.Announcement_spDTO;
@@ -152,3 +215,4 @@ public class Announcement_spServiceImpl implements AnnouncementService {
         return false;
     }
 }
+
