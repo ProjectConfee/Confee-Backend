4a3793e93f480b4ef223977595dce4ad5bbec537
Viraji Dewmini <virajidewmini@gmail.com>
2024-12-01 10:27:57 +0530

    file

--- confee/src/main/java/com/backend/confee/util/config/WebConfig.java
+++ confee/src/main/java/com/backend/confee/util/config/WebConfig.java
@@ -3,6 +3,7 @@ package com.backend.confee.util.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
@@ -10,6 +11,13 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**").allowedOrigins("http://localhost:5173");
+    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        String uploadDir = "file:/Users/virajidewmini/Documents/Confee-Backend/confee/src/main/java/com/backend/confee/uploads/";
+        registry.addResourceHandler("/uploads/**")
+                .addResourceLocations(uploadDir);
     }
 }
 
